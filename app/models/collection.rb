30fe0661e369550c6db28c886309e7456df1b894
Ed Clements <edward.clements@futurelearn.com>
2023-10-04 11:40:13 +0100

    habtm

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,3 @@
 class Collection < ApplicationRecord
+  has_and_belongs_to_many :courses, join_table: :collection_memberships
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,3 @@
 class Course < ApplicationRecord
+  has_and_belongs_to_many :collections, join_table: :collection_memberships
 end

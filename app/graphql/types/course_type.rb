0ac58fe573b0ac66d05b67900c4a21734fda87b1
Maksim Holubeu <4work2mh@gmail.com>
2023-10-06 10:10:33 +0100

    Implement 4 task
        1. Add display the percentage discount alongside the
           courses. As a course could have multiple discounts, ensure that the largest
           possible discount is returned for each course.
        2. Add field discount
        3. -

--- app/graphql/types/course_type.rb
+++ app/graphql/types/course_type.rb
@@ -8,5 +8,10 @@ module Types
     field :published, Boolean, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :discount, Integer, null: false
+
+    def discount
+      object.collections&.map(&:discount)&.map {|discount| discount&.amount.to_i }.max.to_i
+    end
   end
 end
